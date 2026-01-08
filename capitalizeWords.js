dace3e0cf899bf4fe7788bfee1a7817bd72f5607
Abdullah881485 <abdullahalasad106882@gmail.com>
2026-01-08 15:42:59 +0600

    Final

--- capitalizeWords.js
+++ capitalizeWords.js
@@ -3,11 +3,9 @@ function capitalizeWords(str) {
     let result = [];
 
     for (let i = 0; i < words.length; i++) {
-        let word =
-            words[i][0].toUpperCase() + words[i].slice(1);
+        let word = words[i][0].toUpperCase() + words[i].slice(1);
         result.push(word);
     }
-
     return result.join(" ");
 }
 
